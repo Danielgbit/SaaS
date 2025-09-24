9368b10cbc5349954124a4f08dabdfeccc2dba1c
danielgbit <danielbitmobb@gmail.com>
2025-09-24 13:51:56 -0500

    feat: add authentication routes and pages, update dependencies, and refactor Home page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import { BookmarkIcon } from "@radix-ui/react-icons";
+import { Button, Card, Container, Heading } from "@radix-ui/themes";
+
+const LoginPage = () => {
+  return (
+      <Container size='1' height='100'>
+        <Card>
+          <Heading>Login Page</Heading>
+        </Card>
+      </Container>
+  );
+};
+
+export default LoginPage;
